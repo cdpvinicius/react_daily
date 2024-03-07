424f935d5ffb594f0b418d8a2735d028980ee43e
Vinicius Martins <vinicius.martins.cdp@gmail.com>
2024-03-06 22:37:51 -0300

    organazing the repo
    tentando organizar o repositorio criando uma pasta so para o ts

--- dist/TS/tuple.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// only in TS not JS
-// const user: (string | number)[] = ['hc', 1]
-let tUser;
-tUser = ["hc", 1, false];
-let rgb = [0, 0, 0];
-const newUser = [122, "hc@gmail.com"];
-newUser[1] = "hc.com";
