d991207835661de38bcadad78a0a8402ac73ab9d
Vinimrw <vini.mrw@gmail.com>
2024-03-08 16:33:44 -0300

    finish tutorial work

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,48 @@
 // import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Buttons from "./components/Buttons";
+import { useState } from "react";
+
 function App() {
+  const [alertVisivle, setAlertVisivle] = useState(false);
   return (
-    <div className="">
-      <ListGroup></ListGroup>
+    <div>
+      {alertVisivle && (
+        <Alert onClose={() => setAlertVisivle(false)}>My alert</Alert>
+      )}
+      <Buttons color="danger" onClick={() => setAlertVisivle(true)}>
+        prima btn
+      </Buttons>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <Alert>
+//         Mr <span> Hello </span> W
+//       </Alert>
+//     </div>
+//   );
+// }
+
+// import ListGroup from "./components/ListGroup";
+// function App() {
+//   let itens = ["NY", "SF", "JP", "BR", "RU"];
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div className="">
+//       <ListGroup
+//         items={itens}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       ></ListGroup>
+//       {/* <ListGroup></ListGroup> */}
+//     </div>
+//   );
+// }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,58 @@
 // import { Fragment } from "react/jsx-runtime";
+// import { MouseEvent, useState } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  const itens = ["NY", "SF", "JP"];
+// {intem: [], heading:string}
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: strign) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // let itens = ["NY", "SF", "JP", "BR", "RU"];
+  // let selectedIndex = 0;
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // arr[0]; // variable (selectedIndex)
+  // arr[1]; // updater function
+  // const [name, setName] =
+
+  // event handler
+  // const handleClick = (event?: MouseEvent) => console.log(event);
+  // if (itens.length === 0) return <p>No itens Found</p>;
+  // const message = itens.length === 0 ? <p> No item found</p> : null;
+
+  // const getMessage = () => {
+  //   return itens.length === 0 ? <p> No item found</p> : null;
+  // };
 
   return (
     <>
-      <h1>Title</h1>
+      <h1>{heading}</h1>
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {/* <li className="list-group-item">An Iten</li>
         <li className="list-group-item">A second Iten</li>
         <li className="list-group-item">A Third Iten</li>
         <li className="list-group-item">A Four Iten</li> */}
-        {itens.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
@@ -20,3 +60,6 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
+// i dont understood Managing State
+// passing funcions via props
